c54054684db1c0b48693811a5ed12572842fed73
orc <lalanne@gmail.com>
2017-10-28 00:56:36 -0300

    ADD: receiving http GET requests

--- server.go
+++ server.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("------request arrived!-----")
+
+	/*	decoder := json.NewDecoder(r.Body)
+		var req Request
+		err := decoder.Decode(&req)
+		if err != nil {
+			panic(err)
+		}
+
+		io.WriteString(w, buildResponse(computeOfferId(req.Msisdn)))*/
+}
 
 func main() {
 	fmt.Println("vim-go")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler).Methods("GET")
+	http.ListenAndServe(":8000", router)
 }
