d11005ed7937c6d8f4df76cc55e2e2479c879842
orc <lalanne@gmail.com>
2017-11-01 01:03:06 -0300

    ADD: logging in a file

--- server.go
+++ server.go
@@ -5,7 +5,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+func openLogFile(logfile string) {
+	if logfile != "" {
+		lf, err := os.OpenFile(logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+
+		if err != nil {
+			log.Fatal("OpenLogfile: os.OpenFile:", err)
+		}
+
+		log.SetOutput(lf)
+	}
+}
 
 func generic_handler(w http.ResponseWriter, r *http.Request) {
 	log.Println("generic handler")
@@ -26,6 +39,10 @@ func sale_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	logFile := "development.log"
+	openLogFile(logFile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	log.Println("Web service for testing GO")
 
 	router := mux.NewRouter()
