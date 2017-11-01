d5782f8d8f61188ecedf52129d4e0d90523ecc46
orc <lalanne@gmail.com>
2017-11-01 00:45:31 -0300

    ADD: sale and purchase handlers

--- server.go
+++ server.go
@@ -8,8 +8,20 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("------request arrived!-----")
+func generic_handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("generic handler")
+
+	io.WriteString(w, "hello world!")
+}
+
+func purchase_handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("purchase handler")
+
+	io.WriteString(w, "hello world!")
+}
+
+func sale_handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("sale handler")
 
 	io.WriteString(w, "hello world!")
 }
@@ -18,7 +30,9 @@ func main() {
 	fmt.Println("vim-go")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", handler).Methods("GET")
+	router.HandleFunc("/", generic_handler).Methods("GET")
+	router.HandleFunc("/purchase", purchase_handler).Methods("GET")
+	router.HandleFunc("/sale", sale_handler).Methods("GET")
 
 	/*starts an http server*/
 	log.Fatal(http.ListenAndServe(":8000", router))
