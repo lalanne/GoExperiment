3b6feddf03ec5e58498ab35997426b8b59890a9b
orc <lalanne@gmail.com>
2017-12-08 00:09:22 -0300

    ADD: create sql script

--- create.sql
+++ create.sql
@@ -1,2 +1,7 @@
 create database GOTEST;
+
 create table OperationsAllowed (name varchar(20));
+
+insert into OperationsAllowed (name) values ('sale');
+insert into OperationsAllowed (name) values ('purchase');
+insert into OperationsAllowed (name) values ('generic');
