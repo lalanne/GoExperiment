b0616233765214ab19ea007df1d1f40d020be578
Christian Lalanne <lalanne@gmail.com>
2017-12-27 23:30:55 -0300

    ADD: sale and generic

--- test.sql
+++ test.sql
@@ -3,3 +3,5 @@ use GOTEST
 
 create table OperationsAllowed (id varchar(3), error int, host int, op varchar(12));
 insert into OperationsAllowed (id, error, host, op) values ('1', 0, 1, 'purchase');
+insert into OperationsAllowed (id, error, host, op) values ('1', 0, 1, 'sale');
+insert into OperationsAllowed (id, error, host, op) values ('1', 0, 1, 'generic');
