bde23f80c19509de4e19e9d1b25e631a879d91c4
Christian Lalanne Arancibia <clalanne@iMac-de-Ricardo.local>
2017-12-27 23:32:18 -0300

    DEL: sql

--- create.sql
+++
@@ -1,7 +0,0 @@
-create database GOTEST;
-
-create table OperationsAllowed (name varchar(20));
-
-insert into OperationsAllowed (name) values ('sale');
-insert into OperationsAllowed (name) values ('purchase');
-insert into OperationsAllowed (name) values ('generic');
