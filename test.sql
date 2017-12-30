9481c1067309e3bec0c193c6e171f0b5f6aeb790
Christian Lalanne <lalanne@gmail.com>
2017-12-29 23:46:22 -0300

    ADD: select command

--- test.sql
+++ test.sql
@@ -5,3 +5,6 @@ create table OperationsAllowed (id varchar(3), error int, host int, op varchar(1
 insert into OperationsAllowed (id, error, host, op) values ('1', 0, 1, 'purchase');
 insert into OperationsAllowed (id, error, host, op) values ('1', 0, 1, 'sale');
 insert into OperationsAllowed (id, error, host, op) values ('1', 0, 1, 'generic');
+
+/*only if you want to check what you have created*/
+select * from OperationsAllowed;
