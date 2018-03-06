aa323b130290250fb97be70a7bfdcf6d42df509a
orc <lalanne@gmail.com>
2018-03-05 21:54:00 -0300

    CHG: handlers---> api

--- web_service/main.go
+++ web_service/main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
+	"api"
 	"github.com/gorilla/mux"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"regexp"
-	"time"
 )
 
 func main() {
 	logFile := "server_debug.log"
-	handlers.openLogFile(logFile)
+	api.OpenLogFile(logFile)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	log.Println("Web service for testing GO")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", handlers.genericHandler).Methods("GET")
-	router.HandleFunc("/purchase", handlers.purchaseHandler).Methods("GET")
-	router.HandleFunc("/sale", handlers.saleHandler).Methods("GET")
+	router.HandleFunc("/", api.GenericHandler).Methods("GET")
+	router.HandleFunc("/purchase", api.PurchaseHandler).Methods("GET")
+	router.HandleFunc("/sale", api.SaleHandler).Methods("GET")
 
 	/*starts an http server*/
 	log.Fatal(http.ListenAndServe(":8000", router))
