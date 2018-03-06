89b7040f6e0a15b5cf42aae0b13ba3cebddcd15a
orc <lalanne@gmail.com>
2018-03-06 00:25:10 -0300

    ADD: basic testing

---
+++ web_service/src/api/api_test.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	server := httptest.NewServer(Handlers())
+	defer server.Close()
+
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("Received non-200 response: %d\n", resp.StatusCode)
+	}
+
+}
